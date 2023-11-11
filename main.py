54f40f3c53aa033e2589f71ad9f40b2ff57156eb
Karl Sparks <karl@sparks.to>
2023-11-11 22:34:33 +1100

    new bigquery based memory

--- main.py
+++ main.py
@@ -15,27 +15,39 @@ from dotenv import load_dotenv
 import os
 import logging
 
+from src.big_query import database
+
 load_dotenv()
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
 
 intents.message_content = True
+intents.presences = True
 
 client = discord.Client(intents=intents)
 
 prompt = ChatPromptTemplate.from_messages(
     [
         SystemMessage(
-            content="""Sparks-AI, you are a chatbot in a Discord server designed to support and engage with users positively. Conversations will appear with user messages prefixed by their usernames, like 'username: message'. Your role is to:
+            content="""/*
+You are Sparks-AI, a chatbot created to support and engage with users in this Discord server. Your responses should not include metadata such as usernames, channel information, or timestamps. Instead, focus on creating natural and human-like interactions. Adhere to the following directives:
 
-1. Provide Supportive Responses: Offer helpful, constructive, and empathetic interactions.
-2. Ensure Accurate Information: Share information that is factual and logical. If you're unsure about a topic, politely decline to comment.
-3. Maintain Respectful Communication: Treat all users with respect, and promote a positive and inclusive atmosphere in group chats.
-4. Avoid Harmful Content: Do not engage in or propagate harmful, offensive, or inappropriate language or topics.
-5. Recognize Limits: Understand your limitations as an AI. When faced with complex issues, especially those involving conflict or sensitive subjects, respond with caution or advise seeking human assistance.
-6. Adapt to Feedback: Learn from interactions to improve future responses, while adhering to ethical guidelines and user safety."""
+1. Supportive Responses: Provide empathetic and constructive dialogue. Avoid prefixing responses with usernames to maintain a conversational tone.
+
+2. Accurate Information: Offer information that is factual and well-founded. If a topic is beyond your knowledge, politely decline to comment and, if possible, guide users towards reliable sources or professional advice.
+
+3. Respectful Communication: Treat all users with respect, fostering a welcoming and inclusive environment.
+
+4. Harmful Content Avoidance: Do not participate in or propagate any harmful, offensive, or inappropriate content.
+
+5. Limits Recognition: Be aware of your limitations as an AI. In complex or sensitive situations, advise users to seek human assistance from moderators or professionals.
+
+6. Feedback Adaptation: Continuously improve your interactions based on user feedback, always within ethical guidelines and ensuring user safety.
+
+Your goal as Sparks-AI is to enrich the server experience by facilitating engaging, helpful, and respectful conversations.
+*/"""
         ),  # The persistent system prompt
         MessagesPlaceholder(
             variable_name="chat_history"
@@ -48,7 +60,7 @@ prompt = ChatPromptTemplate.from_messages(
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(model="gpt-4-1106-preview")
 
 chat_llm_chain = LLMChain(
     llm=llm,
@@ -60,7 +72,36 @@ chat_llm_chain = LLMChain(
 
 @client.event
 async def on_ready():
-    logging.info("we have logged in as %s", client.user)
+    # Fetch history from guild channels
+    logging.info("Checking Servers")
+    for guild in client.guilds:
+        for channel in guild.channels:
+            if isinstance(channel, discord.TextChannel):
+                try:
+                    async for message in channel.history(
+                        limit=500
+                    ):  # Adjust limit as needed
+                        database.insert_row(message)
+                except discord.errors.Forbidden:
+                    logging.warning(
+                        "Don't have permissions to read %s in %s",
+                        channel.name,
+                        guild.name,
+                    )
+
+    logging.info("Checking DMs")
+    # Fetch history from DM channels
+    for dm_channel in client.private_channels:
+        if isinstance(dm_channel, discord.DMChannel):
+            try:
+                async for message in dm_channel.history(
+                    limit=500
+                ):  # Adjust limit as needed
+                    database.insert_row(message)
+            except discord.errors.Forbidden:
+                logging.warning(
+                    "Don't have permissions to read DM with %s", dm_channel.recipient
+                )
 
 
 @client.event
@@ -70,8 +111,10 @@ async def on_message(msg: Message):
 
     username = str(msg.author).split("#", maxsplit=1)[0]
     user_message = str(msg.content)
+    channel = str(msg.channel)
+    msg_time = str(msg.created_at)
 
-    chat_message = f"{username}: {user_message}"
+    chat_message = f"username: {username} | channel: {channel} | timestamp: {msg_time} | message: {user_message}"
 
     logging.info("Received message: %s", chat_message)
     output = chat_llm_chain.predict(human_input=chat_message)
---
+++ src/big_query.py
@@ -0,0 +1,139 @@
+import logging
+from discord.message import Message
+from abc import ABC, abstractmethod
+from google.cloud import bigquery
+from google.api_core.exceptions import BadRequest
+from typing import Dict, Any
+from dotenv import load_dotenv
+import os
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def message_to_dict(message):
+    # Extracts the necessary information from the Discord Message object
+    # and formats it to match the provided BigQuery schema.
+
+    # Assuming 'message' is a discord.Message object
+    # and 'message.attachments' is a list of discord.Attachment objects
+    # For attachments, we're taking just the first one's URL for this example.
+    return {
+        "message_id": str(message.id),
+        "channel_id": str(message.channel.id),
+        "author_id": str(message.author.id),
+        "author_name": message.author.name,
+        "server_id": str(message.guild.id) if message.guild else "",
+        "message_created_at": message.created_at.isoformat(),
+        "message_content": message.content if message.content else "",
+        "message_reactions": [str(reaction) for reaction in message.reactions]
+        if message.reactions
+        else "",
+        "attachment_id": message.attachments[0].id if message.attachments else "",
+    }
+
+
+# Abstract base class for all database strategies
+class DatabaseStrategy(ABC):
+    @abstractmethod
+    def insert_row(self, data: Message) -> None:
+        pass
+
+    @abstractmethod
+    def get_row_by_id(
+        self, id_value: str, id_name: str = "message_id"
+    ) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def delete_row_by_id(self, id_value: str, id_name: str = "message_id") -> None:
+        pass
+
+
+# Concrete class for BigQuery strategy
+class BigQueryStrategy(DatabaseStrategy):
+    def __init__(self, table_id: str):
+        self.client = bigquery.Client()
+        self.table_id = table_id
+
+    def insert_row(self, data: Message) -> None:
+        insert_data = message_to_dict(data)
+
+        errors = self.client.insert_rows_json(self.table_id, [insert_data])
+
+        if errors:
+            logger.error("Encountered errors while inserting rows: %s", errors)
+        else:
+            logger.info("Row inserted successfully.")
+
+    def get_row_by_id(
+        self, id_value: str, id_name: str = "message_id"
+    ) -> Dict[str, Any]:
+        query = f"""
+            SELECT *
+            FROM `{self.table_id}`
+            WHERE {id_name} = @id_value
+        """
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("id_value", "STRING", id_value)
+            ]
+        )
+        query_job = self.client.query(query, job_config=job_config)
+        result = [dict(row.items()) for row in query_job]
+        if result:
+            logger.info("Retrieved row with %s = %s.", id_name, id_value)
+            return result
+        else:
+            logger.warning("No row found with %s = %s.", id_name, id_value)
+            return None
+
+    def delete_row_by_id(self, id_value: str, id_name: str = "message_id") -> None:
+        query = f"""
+            DELETE FROM `{self.table_id}`
+            WHERE {id_name} = @id_value
+        """
+
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("id_value", "STRING", id_value)
+            ]
+        )
+
+        try:
+            query_job = self.client.query(
+                query, job_config=job_config
+            )  # Make an API request.
+            query_job.result()  # Wait for the job to complete.
+            logger.info("Row with %s = %s deleted successfully.", id_name, id_value)
+
+        except BadRequest as e:
+            logger.error("Error deleting row: %s", e)
+
+
+# Database context that utilizes the strategy
+class DatabaseContext:
+    def __init__(self, strategy: DatabaseStrategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: DatabaseStrategy):
+        self._strategy = strategy
+
+    def insert_row(self, data: Message):
+        self._strategy.insert_row(data)
+
+    def get_row_by_id(self, id_value: str, id_name: str = "message_id"):
+        return self._strategy.get_row_by_id(id_value, id_name)
+
+    def delete_row_by_id(self, id_value: str, id_name: str = "message_id"):
+        self._strategy.delete_row_by_id(id_value, id_name)
+
+
+# Usage
+table_id_str = os.getenv("BIGQUERY_TABLE_ID")
+bigquery_strategy = BigQueryStrategy(table_id=table_id_str)
+
+database = DatabaseContext(strategy=bigquery_strategy)
